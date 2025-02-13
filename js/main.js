ddc724d08b30bfc458ffb24e60365e2671897b18
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-02-13 20:57:05 +0100

    feat: add postcreation and api calls.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,12 @@ import { User } from "./user.js";
 import { Post } from "./post.js";
 import { Comment } from "./comment.js";
 
+const mainContainer = document.getElementById("main-content");
+
 const users = [];
 const comments = [];
 const posts = [];
+let number = 1;
 
 function main() {
   api
@@ -17,12 +20,74 @@ function main() {
           userData.firstName,
           userData.lastName
         );
+        console.log(user);
         users.push(user);
+        renderPosts(user);
       });
+      localStorage.setItem("users", JSON.stringify(users));
     })
     .catch((error) => {
       console.error("Error loading users:", error);
     });
+}
+
+function renderPosts(user) {
+  const postContainer = document.createElement("div");
+  postContainer.classList.add("post-container");
+
+  const postHeading = document.createElement("h3");
+  postHeading.classList.add("post-heading");
+  postHeading.innerText = number;
+  
+
+  const postTopContainer = document.createElement("div");
+  postTopContainer.classList.add("post-top-container");
+
+  const postContent = document.createElement("p");
+
+  const postBottomContainer = document.createElement("div");
+  postBottomContainer.classList.add("post-bottom-container");
+
+  const tagsContainer = document.createElement("div");
+  tagsContainer.classList.add("tags-container")
+
+  const authorContainer = document.createElement("div");
+  authorContainer.classList.add("author-container");
+
+  const authorContent = document.createElement("p");
+  
+
+  const tags = [];
+  for (let i = 0; i < 3; i++) {
+    const tag = document.createElement("div");
+    tag.innerText = "tag " + (i + 1);
+    tags.push(tag);
+  }
+
+  mainContainer.append(postContainer);
+  postContainer.append(postHeading);
+  postContainer.append(postTopContainer);
+  postTopContainer.append(postContent);
+  postContent.innerText = "this is post number: " + number
+  postContainer.append(postBottomContainer);
+  postBottomContainer.append(tagsContainer);
+  tags.forEach((tag) => {
+    tagsContainer.append(tag);
+  })
+
+postBottomContainer.append(authorContainer);
+authorContainer.append(authorContent);
+authorContent.innerText = user.firstName;
+
+
+  
+  
+
+
+
+
+
+  number++;
 }
 
 main();
